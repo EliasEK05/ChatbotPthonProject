dd74558b8ba46a08d1756c4f70daa87912de1563
EliasEK05 <150788188+EliasEK05@users.noreply.github.com>
2023-11-26 20:56:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from function import *
+
+'''
+print("Bienvenue dans menu du chatbot.\nDans ce menu, vous pouvez accéder à différentes focntionnalités")
+while True:
+    print("Voici les différentes fonctionnalités disponible")
+    print("1. Lire un discours")
+    print("2. Consulter la liste des mots les moins importants dans le corpus des disours")
+    print("3. Consulter le(s) mot(s) ayant le score TD-IDF le plus élevé")
+    print("4. Consulter le(s) mot(s) le(s) plus répété(s) par le président Chirac")
+    print("5. Consulter le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et celui qui l’a répété le plus de fois")
+    print("6. Conulster le premier président à parler du climat et/ou de l’écologie")
+    print('7. Consulter le(s) mot(s) que tous les présidents ont évoqués en dehors des mots "non importants" ')
+    user = int(input("Afin d'accéder a la focntionnalité souhaitée, veuillez tapper dans la console le numéro de la fonctionnalité "))
+    if user == 1:
+        print("Quel")
+        '''
